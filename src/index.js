6ef8a63080e3690cbb75dd2f42380fca4d5dc912
localhost <rom.badanin@gmail.com>
2024-10-08 00:10:33 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Electron + React Launcher</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
